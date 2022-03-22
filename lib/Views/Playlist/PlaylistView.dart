6da6f9e58543669379b0879544a739b8f5eb872a
Luca Giovannesi <l.giovannesi@gmail.com>
2022-03-22 13:24:10 +0100

    Added animation on image loading

--- lib/Views/Playlist/PlaylistView.dart
+++ lib/Views/Playlist/PlaylistView.dart
@@ -1,14 +1,24 @@
+import 'dart:math';
+
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:holomusic/Common/DataFetcher/Providers/Playlist.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:holomusic/Common/DataFetcher/VideoInfo.dart';
 import 'package:holomusic/UIComponents/SongItem.dart';
 
-class PlaylistView extends StatelessWidget {
+class PlaylistView extends StatefulWidget {
   Playlist playlist;
   Function()? onBackPressed;
 
   PlaylistView(this.playlist, this.onBackPressed, {Key? key}) : super(key: key);
+
+  @override
+  State<PlaylistView> createState() => _PlaylistViewState();
+}
+
+class _PlaylistViewState extends State<PlaylistView> {
+  double _imageSize = 150;
 
   @override
   Widget build(BuildContext context) {
@@ -19,49 +29,61 @@ class PlaylistView extends StatelessWidget {
         child: Column(children: [
           Row(children: [
             TextButton(
-                onPressed: onBackPressed,
+                onPressed: widget.onBackPressed,
                 child: const Icon(Icons.arrow_back_ios, color: Colors.white))
           ]),
           Expanded(
-              child: ListView(clipBehavior: Clip.antiAlias, children: <Widget>[
-            Column(children: [
-              Container(
-                  decoration: BoxDecoration(
-                      color: playlist.backgroundColor ?? Colors.transparent,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Image.network(
-                    playlist.imageUrl,
-                    width: 150,
-                    height: 150,
-                  )),
-              const SizedBox(height: 15),
-              Text(playlist.name, style: _nameTextStyle),
-              const SizedBox(height: 15),
-              OutlinedButton(
-                onPressed: () {},
-                child: Text(AppLocalizations.of(context)!.follow),
-                style: OutlinedButton.styleFrom(
-                    side: const BorderSide(width: 0.5, color: Colors.white),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50))),
-              ),
-              const SizedBox(height: 15),
-              FutureBuilder<List<VideoInfo>>(
-                future: playlist.getVideosInfo(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return ListBody(
-                      children: snapshot.data!
-                          .map((e) => SongItem(e.title, e.thumbnail, url: e.url))
-                          .toList(),
-                    );
-                  } else {
-                    return const CircularProgressIndicator();
-                  }
-                },
-              )
-            ])
-          ]))
+              child: NotificationListener<ScrollNotification>(
+                  onNotification: (notification) {
+                    setState(() {
+                      _imageSize =
+                          max(150 - notification.metrics.extentBefore, 0);
+                    });
+                    return true;
+                  },
+                  child:
+                      ListView(clipBehavior: Clip.antiAlias, children: <Widget>[
+                    Column(children: [
+                      Container(
+                          decoration: BoxDecoration(
+                              color: widget.playlist.backgroundColor ??
+                                  Colors.transparent,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: ExtendedImage.network(
+                            widget.playlist.imageUrl,
+                            width: _imageSize,
+                            height: _imageSize,
+                          )),
+                      const SizedBox(height: 15),
+                      Text(widget.playlist.name, style: _nameTextStyle),
+                      const SizedBox(height: 15),
+                      OutlinedButton(
+                        onPressed: () {},
+                        child: Text(AppLocalizations.of(context)!.follow),
+                        style: OutlinedButton.styleFrom(
+                            side: const BorderSide(
+                                width: 0.5, color: Colors.white),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50))),
+                      ),
+                      const SizedBox(height: 15),
+                      FutureBuilder<List<VideoInfo>>(
+                        future: widget.playlist.getVideosInfo(),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return ListBody(
+                              children: snapshot.data!
+                                  .map((e) => SongItem(e.title, e.thumbnail,
+                                      url: e.url))
+                                  .toList(),
+                            );
+                          } else {
+                            return const CircularProgressIndicator();
+                          }
+                        },
+                      )
+                    ])
+                  ])))
         ]));
   }
 }
