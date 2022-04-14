6f7977946b33d0fb524ee05e7ee7fc097897bcca
Luca Giovannesi <l.giovannesi@gmail.com>
2022-04-14 12:01:41 +0200

    Autentication unit test finished

--- lib/ServerRequests/User.dart
+++ lib/ServerRequests/User.dart
@@ -1,5 +1,9 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:cookie_jar/cookie_jar.dart';
+import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:holomusic/ServerRequests/PaginatedResponse.dart';
 import 'package:http/http.dart' as http;
 
@@ -23,11 +27,7 @@ class User {
   }
 
   toJson() {
-    return jsonEncode({
-      "id": id,
-      "username": username,
-      "playlist_count": public_playlist_count
-    });
+    return jsonEncode({"id": id, "username": username, "playlist_count": public_playlist_count});
   }
 
   @override
@@ -37,20 +37,81 @@ class User {
 }
 
 class UserRequest {
-  static Future<PaginatedResponse<List<User>>> searchUserByUsername(
-      String username,
+  static var dio = Dio();
+  static var cookieJar = CookieJar();
+
+  static init() {
+    dio.interceptors.add(CookieManager(
+        cookieJar)); //CookieManager stores the sessionCookie for the authenticated request
+  }
+
+  static Future<PaginatedResponse<List<User>>> searchUserByUsername(String username,
       {int page = 0}) async {
     final queryParameters = {"username": username, "page": page.toString()};
-    final uri =
-        Uri.http(ServerParameters.BASE_URL, "searchUser", queryParameters);
+    final uri = Uri.http(ServerParameters.FULL_URL, "searchUser", queryParameters);
     final response = await http.get(uri);
     final jsonObject = jsonDecode(response.body);
     final hasNext = jsonObject['hasNextPage'] as bool;
     final currentPage = jsonObject['currentPage'] as int;
-    final userList = (jsonObject['results'] as List<dynamic>)
-        .map((e) => User.fromJson(e))
-        .toList();
-    return PaginatedResponse(userList,
-        currentPage: currentPage, hasNext: hasNext);
+    final userList = (jsonObject['results'] as List<dynamic>).map((e) => User.fromJson(e)).toList();
+    return PaginatedResponse(userList, currentPage: currentPage, hasNext: hasNext);
+  }
+
+  //Login an user, returns true if success, false otherwise
+  static Future<bool> userLogin(String username, String password) async {
+    final uri = Uri.http(ServerParameters.FULL_URL, "signIn");
+
+    //Get request to get csrf
+    final response0 = await dio.get(uri.toString());
+    final cookie =
+        Cookie.fromSetCookieValue(response0.headers.value("set-cookie")!); //Get the cookie for csrf
+
+    //POST Request
+    final bodyContent = FormData.fromMap(
+        {'username': username, 'password': password, 'csrfmiddlewaretoken': cookie.value});
+    final response1 = await dio.post(uri.toString(),
+        data: bodyContent, options: Options(headers: {"X-CSRFToken": cookie.value}));
+    return response1.statusCode == 200 && response1.data['success'];
+  }
+
+  //Logout an user, returns true on success, false otherwise
+  static Future<bool> logout() async {
+    final uri = Uri.http(ServerParameters.FULL_URL, "logout");
+    final response = await dio.get(uri.toString());
+    return response.statusCode == 200 && response.data['success'];
+  }
+
+  static Future<Map<String, dynamic>> register(String email, String username, String password,
+      [String languageCode = "it-it"]) async {
+    print("REGISTER $email $username $password");
+    final uri = Uri.http(ServerParameters.FULL_URL, "signUp");
+
+    //Get request to get csrf
+    final response0 = await dio.get(uri.toString());
+    final cookie =
+        Cookie.fromSetCookieValue(response0.headers.value("set-cookie")!); //Get the cookie for csrf
+
+    //POST Request
+    final bodyContent = FormData.fromMap({
+      'username': username,
+      'password': password,
+      'email': email,
+      'csrfmiddlewaretoken': cookie.value
+    });
+    final headers = {"X-CSRFToken": cookie.value, "Accept-Language": languageCode};
+    final response1 =
+        await dio.post(uri.toString(), data: bodyContent, options: Options(headers: headers));
+    if (response1.data['success'] != true) {
+      return (response1.data['errors'] as Map<String, dynamic>);
+    } else {
+      return {};
+    }
+  }
+
+  static Future<bool> alreadyExists(String username, [String languageCode = "it-it"]) async {
+    final errors = await register("", username, "");
+
+    return errors.keys.contains("username") &&
+        List<String>.from(errors['username']).any((element) => element.contains("already"));
   }
 }
--- test/server_user_test.dart
+++ test/server_user_test.dart
@@ -2,17 +2,52 @@ import 'package:holomusic/ServerRequests/User.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Fetcher:', () {
+  group('User manager:', () {
+    UserRequest.init();
+
     test('Get user', () async {
       final response = await UserRequest.searchUserByUsername("luca");
       expect(response.result.map((e) => e.username).contains("luca"), true);
     });
+
+    test('Register invalid account', () async {
+      var response = await UserRequest.register("pippo", "pippo", "pippo");
+      expect(response.keys.contains("email"), true);
+
+      response = await UserRequest.register("pippo", "ca", "ca");
+      expect(response.keys.contains("username"), true);
+      expect(response.keys.contains("email"), true);
+      expect(response.keys.contains("password"), true);
+
+      response = await UserRequest.register("pippo@franco.it", "luca", "22");
+      bool alreadyExists =
+          List<String>.from(response['username']).any((element) => element.contains("already"));
+      expect(response.keys.contains("username"), true);
+      expect(alreadyExists, true);
+    });
+
+    /*
+    test('Register', () async {
+      final response =
+          await UserRequest.register("pippo@franco.it", "pippo_franco", "superpassowrd");
+      expect(response.length, 0);
+    });
+     */
+
+    test('Already exists', () async {
+      bool response = await UserRequest.alreadyExists("hackerino");
+      expect(response, true);
+      response = await UserRequest.alreadyExists("pippo_baudo");
+      expect(response, false);
+    });
+
     test('login', () async {
       var response = await UserRequest.userLogin("hackerino", "hackerino");
       expect(response, true);
       response = await UserRequest.userLogin("utente", "non_registrato");
       expect(response, false);
     });
+
     test('logout', () async {
       final response = await UserRequest.logout();
       expect(response, true);
